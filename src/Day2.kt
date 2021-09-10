fa6e92f2c43cb94a7565d806098f1c5a34980bc1
Sabirov Jakhongir <jahon.080879@gmail.com>
2021-09-10 10:34:33 +0500

    Start to learn day 2 of Kotlin Continue

--- src/Day2.kt
+++ src/Day2.kt
@@ -10,11 +10,15 @@ fun main(){
     fun foo(a: Int, b : String = ""){
 
     }
-
+    //var can be assigned
+    var test = 32
+    test ++
+    // val cann't be assigned
+    val test2 =32
+    // test2 ++
     var list = listOf(3,2,52,2);
-
     var evenList = list.filter { it % 2 == 0 }
-
+    val other = list.filter { it % 2 == 0 }
     evenList.forEach { println(it) }
 
     //Read only map
@@ -35,6 +39,29 @@ fun main(){
     for(x in 10 downTo 0){ // from 10 to zero
         println("hello world $x")
     }
+
+    /*
+    lazy() это функция, которая принимает лямбду и возвращает экземпляр класса Lazy<T>,
+    который служит делегатом для реализации ленивого свойства: первый вызов get() запускает лямбда-выражение,
+    переданное lazy() в качестве аргумента, и запоминает полученное значение, а последующие вызовы просто возвращают вычисленное значение.
+     */
+    val erinchoq: String by lazy {
+        println("hello world")
+        println("hello")
+        "fall"
+    }
+
+
+
+    println(erinchoq)
+    println(erinchoq)
+    println(Resource.name)
+    println(Resource.name)
+
 }
 
 data class Customer(val name:String,val email:String)
+
+object Resource{
+    val name = "name"
+}
